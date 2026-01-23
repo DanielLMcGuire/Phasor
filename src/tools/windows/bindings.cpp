a6bbff6004bb0e99102b6f9bc9166a59c0bd7cfe
Daniel McGuire <danielmcguire23@icloud.com>
2026-01-22 19:42:07 -0600

    Added basic win32 handle support

--- src/tools/windows/bindings.cpp
+++ src/tools/windows/bindings.cpp
@@ -20,30 +20,26 @@ struct Function
 	int                lineNumber;
 };
 
-bool isSupportedParam(const std::string &type)
+bool isHandleType(const std::string& type)
 {
+	return type == "HANDLE" || type == "HMODULE" || type == "HWND" ||
+	       type == "HINSTANCE" || type == "HDC";
+}
+
+bool isSupportedParam(const std::string& type)
+{
+	if (isHandleType(type))
+		return true;
+
 	std::string t = type;
 	t.erase(std::remove(t.begin(), t.end(), ' '), t.end());
-	return t == "BOOL" || t == "DWORD" || t == "int" || t == "LONG" || t == "UINT" || t == "ULONG" || t == "float" ||
-	       t == "double" || t == "LPCSTR" || t == "LPSTR" || t == "constchar*" || t == "LPCWSTR" || t == "LPWSTR" ||
-	       t == "constwchar_t*";
+	return t == "BOOL" || t == "DWORD" || t == "int" || t == "LONG" ||
+	       t == "UINT" || t == "ULONG" || t == "float" || t == "double" ||
+	       t == "LPCSTR" || t == "LPSTR" || t == "constchar*" ||
+	       t == "LPCWSTR" || t == "LPWSTR" || t == "constwchar_t*";
 }
 
-std::string mapReturnType(const std::string &type)
-{
-	if (type == "BOOL")
-		return "phasor_make_bool";
-	if (type == "DWORD" || type == "int" || type == "LONG" || type == "UINT" || type == "ULONG")
-		return "phasor_make_int";
-	if (type == "float" || type == "double")
-		return "phasor_make_float";
-	if (type == "LPCSTR" || type == "LPSTR" || type == "constchar*" || type == "LPCWSTR" || type == "LPWSTR" ||
-	    type == "constwchar_t*")
-		return "phasor_make_string";
-	return "";
-}
-
-std::string trim(const std::string &str)
+std::string trim(const std::string& str)
 {
 	size_t start = str.find_first_not_of(" \t\r\n");
 	if (start == std::string::npos)
@@ -52,7 +48,7 @@ std::string trim(const std::string &str)
 	return str.substr(start, end - start + 1);
 }
 
-Function parseFunction(const std::string &line, int lineNumber)
+Function parseFunction(const std::string& line, int lineNumber)
 {
 	Function f;
 	f.rawLine = line;
@@ -64,22 +60,19 @@ Function parseFunction(const std::string &line, int lineNumber)
 	size_t paren = clean.find('(');
 	if (paren == std::string::npos)
 		return f;
+
 	std::string retAndName = clean.substr(0, paren);
 	std::string paramStr = clean.substr(paren + 1, clean.find(')') - paren - 1);
 
 	std::istringstream iss(retAndName);
 	iss >> f.returnType >> f.name;
 
-	// Handle void or empty parameters
 	paramStr = trim(paramStr);
 	if (paramStr.empty() || paramStr == "void")
-	{
-		// No parameters - this is valid
 		return f;
-	}
 
 	std::istringstream pstream(paramStr);
-	std::string        param;
+	std::string param;
 	while (std::getline(pstream, param, ','))
 	{
 		param = trim(param);
@@ -87,35 +80,47 @@ Function parseFunction(const std::string &line, int lineNumber)
 			continue;
 
 		std::istringstream ps(param);
-		Param              p;
+		Param p;
 		ps >> p.type >> p.name;
 
 		if (!isSupportedParam(p.type))
 		{
-			f.returnType = ""; // skip unsupported
+			f.returnType.clear();
 			break;
 		}
+
 		f.params.push_back(p);
 	}
 
 	return f;
 }
 
-void generateWrapper(const Function &f, std::ostream &out)
+void generateWrapper(const Function& f, std::ostream& out)
 {
 	if (f.returnType.empty())
 		return;
 
 	out << "// " << f.rawLine << " @ln:" << f.lineNumber << "\n";
-	out << "static PhasorValue win32_" << f.name << "(PhasorVM* vm, int argc, const PhasorValue* argv) {\n";
+	out << "static PhasorValue win32_" << f.name
+	    << "(PhasorVM* vm, int argc, const PhasorValue* argv) {\n";
 
 	for (size_t i = 0; i < f.params.size(); ++i)
 	{
-		const auto &p = f.params[i];
+		const auto& p = f.params[i];
+
+		if (isHandleType(p.type))
+		{
+			out << "    " << p.type << " " << p.name
+			    << " = HandleSystem::resolve<" << p.type
+			    << ">(phasor_to_int(argv[" << i << "]));\n";
+			continue;
+		}
+
 		std::string conv;
 		if (p.type == "BOOL")
 			conv = "phasor_to_bool(argv[" + std::to_string(i) + "])";
-		else if (p.type == "DWORD" || p.type == "int" || p.type == "LONG" || p.type == "UINT" || p.type == "ULONG")
+		else if (p.type == "DWORD" || p.type == "int" || p.type == "LONG" ||
+		         p.type == "UINT" || p.type == "ULONG")
 			conv = "(" + p.type + ")phasor_to_int(argv[" + std::to_string(i) + "])";
 		else if (p.type == "float" || p.type == "double")
 			conv = "(" + p.type + ")phasor_to_float(argv[" + std::to_string(i) + "])";
@@ -125,33 +130,45 @@ void generateWrapper(const Function &f, std::ostream &out)
 			conv = "(char*)phasor_to_string(argv[" + std::to_string(i) + "])";
 		else if (p.type == "LPCWSTR" || p.type == "constwchar_t*")
 			conv = "(const wchar_t*)phasor_to_string(argv[" + std::to_string(i) + "])";
-		else // LPWSTR
+		else
 			conv = "(wchar_t*)phasor_to_string(argv[" + std::to_string(i) + "])";
+
 		out << "    " << p.type << " " << p.name << " = " << conv << ";\n";
 	}
 
 	out << "    auto result = " << f.name << "(";
 	for (size_t i = 0; i < f.params.size(); ++i)
 	{
-		if (i > 0)
+		if (i)
 			out << ", ";
 		out << f.params[i].name;
 	}
 	out << ");\n";
 
-	std::string wrapFunc = mapReturnType(f.returnType);
-	if (wrapFunc.empty())
-		out << "    return phasor_make_null();\n";
+	if (isHandleType(f.returnType))
+	{
+		out << "    auto id = HandleSystem::store(result);\n";
+		out << "    return phasor_make_int(id);\n";
+	}
+	else if (f.returnType == "BOOL")
+		out << "    return phasor_make_bool(result);\n";
+	else if (f.returnType == "float" || f.returnType == "double")
+		out << "    return phasor_make_float(result);\n";
+	else if (f.returnType == "DWORD" || f.returnType == "int" ||
+	         f.returnType == "LONG" || f.returnType == "UINT" ||
+	         f.returnType == "ULONG")
+		out << "    return phasor_make_int(result);\n";
 	else
-		out << "    return " << wrapFunc << "(result);\n";
+		out << "    return phasor_make_null();\n";
 
 	out << "}\n\n";
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 	std::string inputFile = "winapi.txt";
 	std::string outputFile = "phasor_winapi.cpp";
+
 	if (argc >= 2)
 		inputFile = argv[1];
 	if (argc >= 4 && std::string(argv[2]) == "-o")
@@ -160,28 +177,34 @@ int main(int argc, char **argv)
 	std::ifstream infile(inputFile);
 	std::ofstream outfile(outputFile);
 
-	outfile << "#define PHASOR_FFI_BUILD_DLL\n#include <PhasorFFI.hpp>\n#include <windows.h>\n\n";
+	outfile << "#define PHASOR_FFI_BUILD_DLL\n";
+	outfile << "#include <PhasorFFI.hpp>\n";
+	outfile << "#include <windows.h>\n";
+	outfile << "#include \"../src/tools/windows/handle.hpp\"\n\n";
 
-	std::string           line;
-	int                   lineNumber = 0;
+	std::string line;
+	int lineNumber = 0;
 	std::vector<Function> funcs;
+
 	while (std::getline(infile, line))
 	{
 		lineNumber++;
 		if (line.empty())
 			continue;
+
 		Function f = parseFunction(line, lineNumber);
 		if (!f.returnType.empty())
 			funcs.push_back(f);
 	}
 
-	for (const auto &f : funcs)
+	for (const auto& f : funcs)
 		generateWrapper(f, outfile);
 
 	outfile << "PHASOR_FFI_EXPORT void phasor_plugin_entry(const PhasorAPI* api, PhasorVM* vm) {\n";
-	for (const auto &f : funcs)
+	for (const auto& f : funcs)
 	{
-		outfile << "    api->register_function(vm, \"" << "win32_" << f.name << "\", win32_" << f.name << ");\n";
+		outfile << "    api->register_function(vm, \"win32_" << f.name
+		        << "\", win32_" << f.name << ");\n";
 	}
 	outfile << "}\n";
 }
---
+++ src/tools/windows/handle.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <unordered_map>
+#include <cstdint>
+
+namespace HandleSystem
+{
+using HandleId = uint32_t;
+
+inline HandleId &nextId()
+{
+	static HandleId id = 1;
+	return id;
+}
+
+template <typename T> struct Table
+{
+	static std::unordered_map<HandleId, T> &idToHandle()
+	{
+		static std::unordered_map<HandleId, T> map;
+		return map;
+	}
+
+	static std::unordered_map<T, HandleId> &handleToId()
+	{
+		static std::unordered_map<T, HandleId> map;
+		return map;
+	}
+};
+
+template <typename T> inline HandleId store(T handle)
+{
+	if (!handle)
+		return 0;
+
+	auto &h2i = Table<T>::handleToId();
+	auto &i2h = Table<T>::idToHandle();
+
+	auto it = h2i.find(handle);
+	if (it != h2i.end())
+		return it->second;
+
+	HandleId id = nextId()++;
+	h2i[handle] = id;
+	i2h[id] = handle;
+	return id;
+}
+
+template <typename T> inline T resolve(HandleId id)
+{
+	if (id == 0)
+		return T{};
+
+	auto &i2h = Table<T>::idToHandle();
+	auto  it = i2h.find(id);
+	if (it == i2h.end())
+		return T{};
+
+	return it->second;
+}
+
+template <typename T> inline void remove(HandleId id)
+{
+	auto &i2h = Table<T>::idToHandle();
+	auto &h2i = Table<T>::handleToId();
+
+	auto it = i2h.find(id);
+	if (it == i2h.end())
+		return;
+
+	h2i.erase(it->second);
+	i2h.erase(it);
+}
+} // namespace HandleSystem
