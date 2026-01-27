f88e1aae71f3f20c423ce14e16a019e6f76a5f2d
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2026-01-27 17:32:48 -0600

    Fix: Added missing asm_system_out function
    Signed-off-by: Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>

--- src/Runtime/VM/core/IO/IO.c
+++ src/Runtime/VM/core/IO/IO.c
@@ -98,6 +98,111 @@ int64_t asm_system(const char* cmd) {
 }
 
 char *asm_system_out(const char* cmd) {
+    int argc;
+    char** argv = parse_argv(cmd, &argc);
+    if (!argv) return NULL;
+    char* output = NULL;
+    size_t output_size = 0;
+    size_t output_capacity = 1024;
+    output = malloc(output_capacity);
+    if (!output) {
+        free_argv(argv);
+        return NULL;
+    }
+#ifdef _WIN32
+    SECURITY_ATTRIBUTES sa = { sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };
+    HANDLE hRead, hWrite;
+    if (!CreatePipe(&hRead, &hWrite, &sa, 0)) {
+        free(output);
+        free_argv(argv);
+        return NULL;
+    }
+    STARTUPINFO si = { sizeof(STARTUPINFO) };
+    si.dwFlags = STARTF_USESTDHANDLES;
+    si.hStdOutput = hWrite;       // capture stdout
+    si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+    si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
+    PROCESS_INFORMATION pi;
+    if (!CreateProcess(NULL, (char*)cmd, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+        CloseHandle(hRead);
+        CloseHandle(hWrite);
+        free(output);
+        free_argv(argv);
+        return NULL;
+    }
+    CloseHandle(hWrite);
+    DWORD bytesRead;
+    char buffer[1024];
+    while (ReadFile(hRead, buffer, sizeof(buffer), &bytesRead, NULL) && bytesRead > 0) {
+        if (output_size + bytesRead >= output_capacity) {
+            output_capacity *= 2;
+            char* new_output = realloc(output, output_capacity);
+            if (!new_output) {
+                CloseHandle(hRead);
+                CloseHandle(pi.hProcess);
+                CloseHandle(pi.hThread);
+                free(output);
+                free_argv(argv);
+                return NULL;
+            }
+            output = new_output;
+        }
+        memcpy(output + output_size, buffer, bytesRead);
+        output_size += bytesRead;
+    }
+    output[output_size] = '\0';
+    CloseHandle(hRead);
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+#else
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        free(output);
+        free_argv(argv);
+        return NULL;
+    }
+    pid_t pid = fork();
+    if (pid == -1) {
+        close(pipefd[0]);
+        close(pipefd[1]);
+        free(output);
+        free_argv(argv);
+        return NULL;
+    }
+    if (pid == 0) {
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+        execvp(argv[0], argv);
+        _exit(1);
+    } else {
+        close(pipefd[1]);
+        ssize_t bytesRead;
+        char buffer[1024];
+        while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+            if (output_size + bytesRead >= output_capacity) {
+                output_capacity *= 2;
+                char* new_output = realloc(output, output_capacity);
+                if (!new_output) {
+                    close(pipefd[0]);
+                    free(output);
+                    free_argv(argv);
+                    return NULL;
+                }
+                output = new_output;
+            }
+            memcpy(output + output_size, buffer, bytesRead);
+            output_size += bytesRead;
+        }
+        output[output_size] = '\0';
+        close(pipefd[0]);
+        int status;
+        waitpid(pid, &status, 0);
+    }
+#endif
+    free_argv(argv);
+    return output;
 }
 
 char *asm_system_err(const char* cmd) {
