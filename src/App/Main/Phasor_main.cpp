b7a7c17de0c8d40b16d985efc2b47763bcba0b4e
Daniel McGuire <danielmcguire23@icloud.com>
2026-01-29 00:16:19 -0600

    Improve phasor_main again

--- src/App/Main/Phasor_main.cpp
+++ src/App/Main/Phasor_main.cpp
@@ -31,25 +31,62 @@ std::string readStdin() {
 
 namespace fs = std::filesystem;
 
+void showHelp(fs::path program = "phasor") {
+    std::cout << "Phasor Programming Language\n";
+    std::cout << "Usage: [RAWSCRIPT] |  " << program.stem() << " [SCRIPT, BYTECODE]\n";
+    std::cout << "A. PIPE: <text> | phasor\n";
+    std::cout << "B. JIT/BYTECODE:  phasor <file>\n";
+    std::cout << "C. REPL: phasor\n\n";
+    std::cout << "Example:\n"; 
+#ifdef _WIN32
+    std::cout << "A. CMD:  echo \"print(^\"Hello, World!^\")\" | phasor\n";
+    std::cout << "A. PWSH: echo \"print(`\"Hello, World!`\")\" | phasor\n";
+    std::cout << "B.       phasor hello.phs\n";
+    std::cout << "C.       phasor" << std::endl;
+#else
+    std::cout << "A. echo \"print(\\\"Hello, World!\\\")\" | phasor\n";
+    std::cout << "B. phasor hello.phs\n";
+    std::cout << "C. phasor" << std::endl;
+#endif
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
     try {
         if (!IS_TERMINAL) {
-            std::string source = readStdin();
+            const std::string source = readStdin();
             if (!source.empty()) {
                 Phasor::ScriptingRuntime ScriptRT(argc, argv, envp);
                 Phasor::Frontend::runScript(source);
-                return 0;
+                goto end;
             }
         }
-
         if (argc < 2) {
             Phasor::Repl Repl(argc, argv, envp);
             return Repl.run();
         }
 
-        fs::path filePath = argv[1];
-        std::string ext = filePath.extension().string();
+        const fs::path program = argv[0];
+        const fs::path file = argv[1];
+
+        if (!fs::exists(file)) {
+            const std::string raw = file.string();
+            if (!raw.empty() && (raw.front() == '-' || raw.front() == '/')) {
+                std::string m_path = raw;
+                m_path.erase(0, m_path.find_first_not_of("-/"));
+                if (m_path == "help" || m_path == "h" || m_path == "?" || m_path == "h" || m_path == "help") {
+                    showHelp(program);
+                    goto end;
+                }
+                std::cerr << "Invalid argument: " << m_path << "\n";
+            } else 
+                std::cerr << "File not found: " << raw << "\n";
+
+            goto err;
+        }
+
+
+        const std::string ext = file.extension().string();
 
         if (ext == ".phs") {
             Phasor::ScriptingRuntime ScriptRT(argc, argv, envp);
@@ -59,10 +96,10 @@ int main(int argc, char *argv[], char *envp[])
             return BinRT.run();
         } else if (ext == ".phir") {
             std::cout << "Phasor IR (.phir) compilation not yet implemented.\n";
-            return 0;
+            goto end;
         } else {
             std::cerr << "Unknown extension: " << ext << "\n";
-            return 1;
+            goto err;
         }
     }
     catch (const std::exception &e)
@@ -71,5 +108,8 @@ int main(int argc, char *argv[], char *envp[])
         return 1;
     }
 
+end:
     return 0;
+err:
+    return 1;
 }
