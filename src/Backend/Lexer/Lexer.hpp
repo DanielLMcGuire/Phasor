9aa73934fc537429b19f69b6dab1e2c430fe78f2
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2026-01-29 01:22:02 -0600

    Update Lexer.hpp
    Signed-off-by: Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>

--- src/Backend/Lexer/Lexer.hpp
+++ src/Backend/Lexer/Lexer.hpp
@@ -42,7 +42,7 @@ class Lexer
 	char  advance();
 	bool  isAtEnd();
 	void  skipWhitespace();
-	void  skipShebang()
+	void  skipShebang();
 	Token scanToken();
 	Token identifier();
 	Token number();
