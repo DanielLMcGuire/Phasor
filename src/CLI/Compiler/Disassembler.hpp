7565b101d029f17ac950db1f67f5a5b61ad472da
Daniel McGuire <danielmcguire23@icloud.com>
2026-02-07 22:58:15 -0600

    Added disassembler

---
+++ src/CLI/Compiler/Disassembler.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+
+namespace Phasor
+{
+
+/**
+ * @class Disassembler
+ * @brief CLI wrapper for dissassembling Phasor binaries
+ */
+class Disassembler
+{
+  public:
+	Disassembler(int argc, char *argv[]);
+	int run();
+
+  private:
+	struct Args
+	{
+		std::filesystem::path inputFile;
+		std::filesystem::path outputFile;
+		std::string           programName;
+		bool                  noLogo = false;
+		bool                  showHelp = false;
+	} m_args;
+
+	bool parseArguments(int argc, char *argv[]);
+	const bool showHelp();
+	const bool decompileBinary();
+};
+
+} // namespace Phasor
