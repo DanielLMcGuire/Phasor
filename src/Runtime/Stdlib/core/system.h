a581c8373f2d5c632df4fbe404eedb2f01fd0e82
Daniel McGuire <danielmcguire23@icloud.com>
2026-02-17 15:05:02 -0600

    Fix BSD support

--- src/Runtime/Stdlib/core/system.h
+++ src/Runtime/Stdlib/core/system.h
@@ -1,11 +1,16 @@
 #ifndef CORE_SYSTEM_H
 #define CORE_SYSTEM_H	
 
-#if defined(_WIN32)
+#ifdef _WIN32
 #include <windows.h>
-#elif defined(__APPLE__)
+#elif defined(__APPLE__) && defined(__MACH__)
 #include <mach/mach.h>
-#else
+#include <mach/host_info.h>
+#elif defined(__linux__)
+#include <unistd.h>
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #include <unistd.h>
 #endif
 
