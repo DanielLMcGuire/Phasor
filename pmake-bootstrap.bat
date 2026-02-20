8c7b15469885a410a2148256e17500a6a5e5b60e
Daniel McGuire <danielmcguire23@icloud.com>
2026-02-19 21:06:18 -0600

    move out pmake

--- pmake-bootstrap.bat
+++ pmake-bootstrap.bat
@@ -1,12 +1,13 @@
 @echo off
 setlocal
+git submodule update
 set SCRIPT_DIR=%~dp0
 set TMP_DIR=%SCRIPT_DIR%.tmp
 cmake -S %SCRIPT_DIR% -B %TMP_DIR% -DCMAKE_BUILD_TYPE=Release -G Ninja 2>&1 1>nul
 echo Building...
 ninja -C %TMP_DIR% pmake 2>&1 1>nul
 echo Copying files...
-copy %TMP_DIR%\src\Executable\utils\pmake\pmake.exe %SCRIPT_DIR%pmake.exe /Y 2>&1 1>nul
+copy %TMP_DIR%\thirdparty\pmake\Executable\pmake\pmake.exe %SCRIPT_DIR%pmake.exe /Y 2>&1 1>nul
 echo Cleaning up...
 rmdir /s /q %TMP_DIR% 2>&1 1>nul
 endlocal
--- pmake-bootstrap.sh
+++ pmake-bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+git submodule update
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TMP_DIR="$SCRIPT_DIR/.tmp"
 cmake -S "$SCRIPT_DIR" -B "$TMP_DIR" -DCMAKE_BUILD_TYPE=Release -G Ninja 2>&1 1>/dev/null
 echo "Building..."
 ninja -C "$TMP_DIR" pmake 2>&1 1>nul
 echo  "Copying files..."
-cp "$TMP_DIR/src/Executable/utils/pmake/pmake.exe" "$SCRIPT_DIR/pmake.exe" 2>&1 1>/dev/null
+cp "$TMP_DIR/thirdparty/pmake/Executable/pmake/pmake" "$SCRIPT_DIR/pmake.exe" 2>&1 1>/dev/null
 echo "Cleaning up..."
 rm -rf "$TMP_DIR" 2>&1 1>/dev/null
