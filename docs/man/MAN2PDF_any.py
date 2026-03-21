2ddc0223c092b4c1d2dbda4bf93eec1d4e3e4963
Daniel <danielmcguire23@icloud.com>
2026-03-20 23:42:11 -0500

    Add check for docker
    Signed-off-by: Daniel <danielmcguire23@icloud.com>

--- docs/man/MAN2PDF_any.py
+++ docs/man/MAN2PDF_any.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import subprocess
 import os
+import sys
+
+if subprocess.run(["docker", "--version"], capture_output=True).returncode != 0:
+    print("Error: Docker is not installed. Please install it from https://docker.com/desktop", file=sys.stderr)
+    sys.exit(1)
 
 subprocess.run([
     "docker", "run", "--rm",
