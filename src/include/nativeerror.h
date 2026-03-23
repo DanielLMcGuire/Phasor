586111da0f3c6422a1a236f7e3d72b5dd94beb2f
Daniel <danielmcguire23@icloud.com>
2026-03-22 23:01:51 -0500

    Update nativeerror.h
    Signed-off-by: Daniel <danielmcguire23@icloud.com>

--- src/include/nativeerror.h
+++ src/include/nativeerror.h
@@ -6,5 +6,5 @@
 	MessageBoxA(NULL, std::string(msg).c_str(), "Phasor VM Runtime Error", MB_OK | MB_ICONERROR); \
 	fprintf(stderr, "Error: %s\n", msg)
 #else
-#define error(msg) fprintf(stderr, "Error: %s\n", msg)
+#define error(msg) fprintf(stderr, "Error: %s\n", std::string(msg).c_str())
 #endif
