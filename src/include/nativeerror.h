8e8678840527dfedd8dcb9be06f1aa15f2a89715
Daniel McGuire <danielmcguire23@icloud.com>
2026-03-22 22:59:32 -0500

    Use cstdio instead of iostream for nativeerror.h

--- src/include/nativeerror.h
+++ src/include/nativeerror.h
@@ -1,8 +1,10 @@
+#include <cstdio>
+
 #ifdef _WIN32
 #include <Windows.h>
 #define error(msg) \
 	MessageBoxA(NULL, std::string(msg).c_str(), "Phasor VM Runtime Error", MB_OK | MB_ICONERROR); \
-	std::cerr << "Error: " << msg << std::endl
+	fprintf(stderr, "Error: %s\n", msg)
 #else
-#define error(msg) std::cerr << "Error: " << msg << std::endl
+#define error(msg) fprintf(stderr, "Error: %s\n", msg)
 #endif
