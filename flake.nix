01a9893a9b3a4bbed67571a96d7b2653364964d1
Daniel McGuire <danielmcguire23@icloud.com>
2026-03-28 22:24:53 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   description = "Phasor";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -18,6 +16,11 @@
           pkgs.cmake
           pkgs.ninja
         ];
+        preConfigure = ''
+          export AR="gcc-ar"
+          export RANLIB="gcc-ranlib"
+          export NM="gcc-nm"
+        '';
       };
     };
 }
