98d63e4c8cf7c918002063f078fa5b0eaf2f2324
Daniel McGuire <danielmcguire23@icloud.com>
2026-04-04 04:43:21 -0500

    Remove unused object

--- src/Executable/Main/Phasor/Phasor_main.cpp
+++ src/Executable/Main/Phasor/Phasor_main.cpp
@@ -62,7 +62,6 @@ int main(int argc, char *argv[], char *envp[])
 			const std::string source = readStdin();
 			if (!source.empty())
 			{
-				Phasor::ScriptingRuntime ScriptRT(argc, argv, envp);
 				return Phasor::Frontend::runScript(source, nullptr, "");
 			}
 		}
