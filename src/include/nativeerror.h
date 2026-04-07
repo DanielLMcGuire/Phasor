3e02c98e8bec2e9a8971dae7eb7449c263b22861
Daniel McGuire <danielmcguire23@icloud.com>
2026-04-07 12:03:50 -0500

    Add macos native messagebox in debug config

--- src/include/nativeerror.h
+++ src/include/nativeerror.h
@@ -1,15 +1,34 @@
+#pragma once
 #include <print>
 #include <iostream>
+#include <string>
 
-#ifndef SANDBOXED
-	#ifdef _WIN32
-		#include <Windows.h>
-		#define error(msg) \
-			MessageBoxA(NULL, std::string(msg).c_str(), "Phasor VM Runtime Error", MB_OK | MB_ICONERROR); \
-			std::println(std::cerr, "Error: {}", msg)
-	#else
-		#define error(msg) std::println(std::cerr, "Error: {}", msg)
-	#endif
-#else
-	#define error(msg) std::println(std::cerr, "Error: {}", msg)
+#define _phs_nativeerror_console(msg) std::println(std::cerr, "Error: {}", msg)
+
+#define error(msg) _phs_nativeerror_console(msg)
+
+#if !defined(SANDBOXED) && defined(_DEBUG)
+    #if defined(_WIN32)
+        #include <Windows.h>
+        #undef error
+        #define error(msg) \
+            do { \
+                std::string _msg = (msg); \
+                MessageBoxA(NULL, _msg.c_str(), "Phasor VM Runtime Error", MB_OK | MB_ICONERROR); \
+                _phs_nativeerror_console(_msg); \
+            } while (0)
+    #elif defined(__APPLE__)
+        #include <CoreFoundation/CoreFoundation.h>
+        #undef error
+        #define error(msg) \
+            do { \
+                std::string _msg = (msg); \
+                CFStringRef _cfMsg = CFStringCreateWithCString(NULL, _msg.c_str(), kCFStringEncodingUTF8); \
+                CFUserNotificationDisplayAlert( \
+                    0, kCFUserNotificationStopAlertLevel, NULL, NULL, NULL, \
+                    CFSTR("Phasor VM Runtime Error"), _cfMsg, CFSTR("OK"), NULL, NULL, NULL); \
+                CFRelease(_cfMsg); \
+                _phs_nativeerror_console(_msg); \
+            } while (0)
+    #endif
 #endif
