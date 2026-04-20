f07dcd678b945676a0c8cc079652f18b086b8067
Daniel McGuire <danielmcguire23@icloud.com>
2026-04-20 00:13:26 -0500

    Isolate powershell dotnet code

--- src/Extensions/powershell/Phasor/Phasor.psm1
+++ src/Extensions/powershell/Phasor/Phasor.psm1
@@ -1,42 +1,8 @@
 #Requires -Version 5.1
 
 if (-not ("PHASOR_INTERNAL_ABI_3_2_0" -as [type])) {
-    Add-Type @"
-using System;
-using System.Runtime.InteropServices;
-public static class PHASOR_INTERNAL_ABI_3_2_0 {
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int evaluatePHS(IntPtr vm, string script, string moduleName, string modulePath, bool verbose);
-
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int evaluatePUL(IntPtr vm, string script, string moduleName);
-
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern int exec(IntPtr vm, byte[] bytecode, UIntPtr bytecodeSize, string moduleName, int argc, string[] argv);
-
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern IntPtr createState();
-
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool freeState(IntPtr vm);
-
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern void initStdLib(IntPtr vm);
-
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool resetState(IntPtr vm, bool resetFunctions, bool resetVariables);
-
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool compilePHS(string script, string moduleName, string modulePath,
-                                         byte[] buffer, UIntPtr bufferSize, out UIntPtr outSize);
-
-    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
-    public static extern bool compilePUL(string script, string moduleName,
-                                         byte[] buffer, UIntPtr bufferSize, out UIntPtr outSize);
+    Add-Type -Path "$PSScriptRoot\PhasorABI.cs"
 }
-"@
-}
-
 
 $script:_PhasorTrackedStates = [System.Collections.Generic.List[PSCustomObject]]::new()
 
---
+++ src/Extensions/powershell/Phasor/PhasorABI.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Runtime.InteropServices;
+
+public static class PHASOR_INTERNAL_ABI_3_2_0 {
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int evaluatePHS(IntPtr vm, string script, string moduleName, string modulePath, bool verbose);
+
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int evaluatePUL(IntPtr vm, string script, string moduleName);
+
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int exec(IntPtr vm, byte[] bytecode, UIntPtr bytecodeSize, string moduleName, int argc, string[] argv);
+
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr createState();
+
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool freeState(IntPtr vm);
+
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void initStdLib(IntPtr vm);
+
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool resetState(IntPtr vm, bool resetFunctions, bool resetVariables);
+
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool compilePHS(string script, string moduleName, string modulePath,
+                                         byte[] buffer, UIntPtr bufferSize, out UIntPtr outSize);
+
+    [DllImport("phasorrt.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern bool compilePUL(string script, string moduleName,
+                                         byte[] buffer, UIntPtr bufferSize, out UIntPtr outSize);
+}
