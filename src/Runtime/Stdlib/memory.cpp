f12ad85336b8a37fbfe266f4a77669d7e4a84056
Daniel McGuire <danielmcguire23@icloud.com>
2026-05-19 07:27:26 -0500

    somehow forgot to name free() free

--- src/Runtime/Stdlib/memory.cpp
+++ src/Runtime/Stdlib/memory.cpp
@@ -5,17 +5,17 @@ namespace Phasor
 
 void StdLib::registerMemoryFunctions(VM *vm)
 {
-	vm->registerNativeFunction("stdmem", StdLib::var_free);
+	vm->registerNativeFunction("free", StdLib::var_free);
 }
 
 Value StdLib::var_free(const std::vector<Value> &args, VM *vm)
 {
-	checkArgCount(args, 1, "stdmem");
+	checkArgCount(args, 1, "free");
 
 	const Value &arg = args[0];
 
 	if (!arg.isString())
-		throw std::runtime_error("stdmem(): argument must be a string");
+		throw std::runtime_error("free(): argument must be a string");
 
 	vm->freeVariableByName(arg.asString());
 	return Value();
