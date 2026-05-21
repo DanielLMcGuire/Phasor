368312a003a9415b9f45e55ca802dc08336bcead
Daniel McGuire <danielmcguire23@icloud.com>
2026-05-21 04:03:37 -0500

    Update phasorrt-rs example

--- examples/rust/bin.rs
+++ examples/rust/bin.rs
@@ -6,16 +6,36 @@ fn main() -> Result<(), PhasorError> {
     let mut vm = PhasorVM::new()?;
     vm.init_stdlib()?;
 
-    let script = "fn main() -> int { using(\"stdio\", \"stdsys\"); puts(\"Hello, World!\"); return 0;} shutdown(main());";
+    let script = "using(\"stdsys\");
+                        fn main() -> int {
+                            using(\"stdio\");
+                            puts(\"Hello, World!\");
+                            return 0;
+                        }
+                        shutdown(main());";
     let bc: Vec<u8> = compile_phs(script, "rusttest", Some(""))?;
 
     // args are command line, i might add function args in a future C API version
-    match vm.exec_func_int(&bc, "rusttest", "main", &[]) {
+    match vm.exec_func_int(&bc, "exec_func_int", "main", &[]) {
         Ok(code) => {
             if code == 0 {
-                println!("exec_func_int executed successfully!");
+                println!("exec_func_int executed successfully");
             } else {
                 println!("Function returned code: {}", code);
+            }
+        }
+
+        Err(err) => {
+            eprintln!("VM execution failed: {:?}", err);
+        }
+    }
+
+    match vm.evaluate_phs(script, "evaluate_phs", None, false) {
+        Ok(code) => {
+            if code == 0 {
+                println!("evaluate_phs executed successfully");
+            } else {
+                println!("evaluate_phs returned code: {}", code);
             }
         }
 
