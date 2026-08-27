1e51b1af100b3181b70c464519267f71842ef0a5
Daniel McGuire <danielmcguire23@icloud.com>
2026-08-27 08:02:15 -0500

    fix build

--- src/Runtime/VM/VM.cpp
+++ src/Runtime/VM/VM.cpp
@@ -2,13 +2,11 @@
 #include "VM.hpp"
 #endif
 
-#ifdef TRACING
 #ifdef PHASOR_USES_BOOST
 	#include <boost/assert/source_location.hpp>
 	#define PHS_SRC_LOC() (std::format("{} @ {}:{}", BOOST_CURRENT_LOCATION.function_name(), BOOST_CURRENT_LOCATION.file_name(), BOOST_CURRENT_LOCATION.line()))
 #else
 	#define PHS_SRC_LOC() (std::format("VM::{}()", __func__))
-#endif
 #endif
 
 namespace Phasor
